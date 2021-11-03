373962d7005cf17d0f703908e7b5a5dc8eda39f3
kumattau <kumattau@gmail.com>
2021-11-03 23:13:16 +0900

    Update

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
 from setuptools import setup
-setup()
+
+version_py = "greetings/greetings.py"
+
+__version__ = None
+with open(version_py, "r") as file:
+    for line in file:
+        if line.startswith("__version__"):
+            exec(line)
+            break
+assert __version__ is not None
+
+setup(version=__version__)
