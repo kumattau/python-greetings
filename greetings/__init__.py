0898f49823970787c597fa4fafecd3b8ca0608db
kumattau <kumattau@gmail.com>
2021-11-04 00:20:53 +0900

    Update

--- greetings/__init__.py
+++ greetings/__init__.py
@@ -4,5 +4,5 @@
 # Use of this source code is governed by a MIT License
 #
 
-from .greetings import __doc__, __all__, __author__, __copyright__, __email__, __license__, __version__
+from .greetings import __doc__, __all__, __version__
 from .greetings import *
