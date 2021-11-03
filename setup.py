ad0d20c6e4c2c3a2acc02b586b0e1165e232c5c1
kumattau <kumattau@gmail.com>
2021-11-04 00:19:16 +0900

    Update

--- setup.py
+++ setup.py
@@ -10,8 +10,5 @@ def get_metadata(file, key):
 metadata_file = "greetings/greetings.py"
 
 setup(
-    author=get_metadata(metadata_file, "__author__"),
-    author_email=get_metadata(metadata_file, "__email__"),
     version=get_metadata(metadata_file, "__version__"),
-    license=get_metadata(metadata_file, "__license__"),
 )
