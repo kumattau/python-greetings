5ba25de239dd5d8e4b9811aa408428c02aebefa8
kumattau <kumattau@gmail.com>
2021-11-03 15:40:47 +0900

    Update

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,5 +1,11 @@
+#
+# Copyright (c) 2021 kumattau
+#
+# Use of this source code is governed by a MIT License
+#
 
 import hello
+
 
 def test_hello():
     assert hello.say_hello() == "hello, world"
